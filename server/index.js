14b9274199baed593768e85ab18d8eb6ae12fecf
Burak <mburak.demir.059@gmail.com>
2022-04-23 16:04:04 +0300

    Mongodb Connection

--- server/index.js
+++ server/index.js
@@ -5,8 +5,15 @@ const cors=require('cors');
 
 const app=express();
 
+mongoose.connect("mongodb+srv://user31:Burakdemir99@tasks.g0tpu.mongodb.net/Tasks?retryWrites=true&w=majority",{
+    useNewUrlParser:true,
+})
+
 app.use(express.json());
 app.use(cors());
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("tabi efendim");
